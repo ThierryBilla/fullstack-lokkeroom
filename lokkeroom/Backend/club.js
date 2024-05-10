2a824791169559e601326a2be6a273c5597e369e
Myriiam <kadiimyriam@hotmail.com>
2024-05-10 15:45:57 +0200

    Change cors + add functional logout button

--- lokkeroom/Backend/club.js
+++ lokkeroom/Backend/club.js
@@ -9,7 +9,7 @@ const routes = require('./routes');
 const server = express();
 
 // Configure CORS
-server.use(cors({
+/* server.use(cors({
   origin: function (origin, callback) {
       const allowedOrigins = ['http://localhost:5173', 'http://127.0.0.1:5173'];
       if (!origin || allowedOrigins.indexOf(origin) !== -1) {
@@ -20,9 +20,9 @@ server.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE']
-}));
+})); */
 
-
+server.use(cors());
 server.use(express.json()); // Ensure this middleware is used if you're parsing JSON bodies
 server.use('/', routes);
 
